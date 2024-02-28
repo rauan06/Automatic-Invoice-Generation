d51ec6648170e5e5eb6c03efdc82af25cac72a77
Rauan <138851982+rauan06@users.noreply.github.com>
2024-02-28 13:11:09 +0600

    Installed weasyprint successfully

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
+import os
+
+os.add_dll_directory(r"C:\Users\ommir\OneDrive\Documents\GitHub\Automatic-Invoice-Generation")
+
 from weasyprint import HTML
 import flask
+
+html = HTML('invoice.html')
+html.write_pdf('invoice.pdf')
